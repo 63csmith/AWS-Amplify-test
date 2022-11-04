995ff1204e0d2e3a6305422861525151fe8884ce
63csmith <63csmith@gmail.com>
2022-11-04 13:05:58 -0600

    file upload test for AWS

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3teststorage9ab3c180": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ import ContactUs from "./components/pages/ContactUs";
 
 import "./App.css";
 import Form from "./components/pages/Form";
-
+//AWS S3 Bucket
+import { Storage } from "aws-amplify";
 //AWS Auth
 
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import { useState } from "react";
 
 function App({ signOut }) {
+  const [fileData, setFileData] = useState();
+  const [fileStatus, setFileStatus] = useState(false);
+
+  const uploadFile = async () => {
+    const result = await Storage.put(fileData.name, fileData, {
+      contentType: fileData.type,
+    });
+    setFileStatus(true);
+    console.log(21, result);
+  };
+
   return (
     <div className="body-wrap">
       <button onClick={signOut}>Sign Out</button>
+      <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
+      <button onClick={uploadFile}>Upload</button>
+      {fileStatus ? "File Uploaded" : ""}
       <Router>
         <Layout>
           <Switch>
